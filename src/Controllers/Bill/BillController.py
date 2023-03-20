cd700718bd639eb271f63827118b263cf2dfa3e0
GuilhermeMelow <guilhermefepeme@hotmail.com>
2023-03-20 16:55:08 -0300

    Removendo classe de requisição desnecessária.

--- src/Controllers/Bill/BillController.py
+++ src/Controllers/Bill/BillController.py
@@ -1,9 +1,8 @@
 
 from uuid import UUID
+from dateutil.parser import parse
+from flask import Flask, Request, jsonify, request, make_response
 
-from flask import Flask, jsonify, request, make_response
-
-from src.Controllers.Bill.BillRequest import BillRequest
 from src.Controllers.Bill.BillResponse import BillResponse
 from src.Models.Bill import Bill
 from src.Repositories.BillRepository import BillRepository
@@ -27,8 +26,7 @@ def bill_controller(app: Flask):
 
     @app.post("/bills")
     def bills_register():
-        bill_request = BillRequest.create(request.get_json())
-        bill = __map_bill(bill_request)
+        bill = __map_bill(request)
 
         repository.add(bill)
         result = BillResponse.create(bill)
@@ -37,16 +35,17 @@ def bill_controller(app: Flask):
 
     @app.put("/bills/<bill_id>")
     def bills_update(bill_id):
-        bill_request = BillRequest.create(request.get_json(), bill_id)
-        bill = __map_bill(bill_request)
+        bill = __map_bill(request, bill_id)
 
         repository.update(UUID(bill_id), bill)
         result = BillResponse.create(bill)
 
         return make_response((jsonify(result)), 201)
 
-    def __map_bill(bill_request: BillRequest):
-        return Bill(description=bill_request.description,
-                    due_date=bill_request.due_date,
-                    price=bill_request.price,
-                    id=bill_request.id)
+    def __map_bill(request: Request, bill_id=None):
+        dct: dict[str, str] = request.get_json()
+
+        return Bill(description=dct["description"],
+                    due_date=parse(dct["due_date"]),
+                    price=float(dct["price"]),
+                    id=bill_id)
