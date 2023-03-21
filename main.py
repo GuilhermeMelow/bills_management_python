80f3f6d6f33687102cd774f61ab4810cbc0b7713
GuilhermeMelow <guilhermefepeme@hotmail.com>
2023-03-21 12:26:53 -0300

    Separando controller.

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
+from src.Controllers.Auth.AuthController import auth_controller
 from src.Controllers.Bill.BillController import bill_controller
 
 from os import environ as env
 from dotenv import load_dotenv
-from urllib.parse import quote_plus, urlencode
 
-from flask import Flask, redirect, session, url_for
+from flask import Flask
 from authlib.integrations.flask_client import OAuth
 
 
@@ -14,42 +14,6 @@ load_dotenv()
 def create_app():
     app = Flask(__name__)
     app.secret_key = env.get("APP_SECRET_KEY")
-
-    oauth = OAuth(app)
-    oauth.register(
-        name="bills_management",
-        overwrite=True,
-        client_id=env.get("AUTH0_CLIENT_ID"),
-        client_secret=env.get("AUTH0_CLIENT_SECRET"),
-        client_kwargs={
-            "scope": "openid profile email",
-        },
-        server_metadata_url=f'https://{env.get("AUTH0_DOMAIN")}/.well-known/openid-configuration'
-    )
-
-    @app.get("/login")
-    def login():
-        return oauth.bills_management.authorize_redirect(
-            redirect_uri=url_for("callback", _external=True),
-        )
-
-    @app.get("/callback")
-    def callback():
-        token = oauth.bills_management.authorize_access_token()
-        session["user"] = token
-
-        return redirect("/")
-
-    @app.get("/logout")
-    def logout():
-        session.clear()
-
-        encode = urlencode({
-            "returnTo": url_for("home", _external=True),
-            "client_id": env.get("AUTH0_CLIENT_ID"),
-        }, quote_via=quote_plus)
-
-        return redirect(f'https://{env.get("AUTH0_DOMAIN")}/v2/logout?{encode}')
 
     @app.get("/")
     def home():
@@ -61,6 +25,7 @@ def create_app():
 if __name__ == "__main__":
     app = create_app()
 
+    auth_controller(app)
     bill_controller(app)
 
     app.run(host="0.0.0.0", port=5002)
