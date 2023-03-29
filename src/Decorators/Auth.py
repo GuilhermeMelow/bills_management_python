c976d1a929d35d6fcd42d175f73a899d68e1d8dd
GuilhermeMelow <guilhermefepeme@hotmail.com>
2023-03-29 16:41:05 -0300

    Autenticando requisição do usuário e salvando claims.

--- src/Decorators/Auth.py
+++ src/Decorators/Auth.py
@@ -1,18 +1,69 @@
-from flask import request, session
-from jwt import decode
+from attr import dataclass
+from flask import request
+from os import environ as env
+import flask
+from requests import request as send_request
+from jose import jwt
+
+
+@dataclass(init=True)
+class AuthException(Exception):
+    error: str
+    code: int = 401
+
+
+def get_jwt_token(raw: str):
+    parts = raw.split()
+
+    if not parts[0].lower() == "bearer":
+        raise AuthException("Not contains bearer assigned")
+
+    if parts.__len__() != 2:
+        raise AuthException("Incorrect token format")
+
+    return parts[1]
+
+
+def get_rsa_key(token):
+    jwks = send_request(
+        url=f"https://{env.get('AUTH0_DOMAIN')}/.well-known/jwks.json",
+        method="get").json()
+
+    token_headers = jwt.get_unverified_header(token)
+    key = next((jwk for jwk in jwks["keys"]
+                if jwk["kid"] == token_headers["kid"]), None)
+
+    return {
+        "kty": key["kty"],
+        "n": key["n"],
+        "e": key["e"]
+    }
 
 
 def auth(func):
     def wrapper(*args, **kwargs):
-        user_session = session.get("user")
-        access_token = user_session['access_token']
+        raw = request.headers.get("Authorization")
+        token = get_jwt_token(raw)
 
-        if (user_session is None):
-            raise Exception("It's not possible to show this content for you.")
+        try:
+            payload = jwt.decode(
+                token,
+                key=get_rsa_key(token),
+                algorithms=env.get("AUTH0_ALGORITHMS"),
+                audience=env.get("AUTH0_API_AUDIENCE"),
+                issuer=f"https://{env.get('AUTH0_DOMAIN')}/")
 
-        result = decode(access_token, "secret", ["HS256"])
-        print(result)
+            flask.session["user_claims"] = payload
 
-        func(*args, **kwargs)
+        except jwt.ExpiredSignatureError:
+            raise AuthException("Token is expired.")
+
+        except jwt.JWTClaimsError:
+            raise AuthException("Incorrect claims, check aud and issuer.")
+
+        except Exception:
+            raise AuthException("Unable for authentication.")
+
+        return func(*args, **kwargs)
 
     return wrapper
