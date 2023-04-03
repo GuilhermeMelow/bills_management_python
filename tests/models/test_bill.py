1eebd4db833ce0200f5b59ff10b2adf1ad7ef183
GuilhermeMelow <Guilhermefepeme@hotmail.com>
2023-04-02 23:25:06 -0300

    Corrigindo teste Bill.

--- tests/models/test_bill.py
+++ tests/models/test_bill.py
@@ -7,13 +7,13 @@ from src.Models.Bill import Bill
 class BillTest(unittest.TestCase):
     def test_create_with_price(self):
         price = 120.22
-        bill = Bill("Teste", datetime.now(), price)
+        bill = Bill(description="Teste", due_date=datetime.now(), price=price)
 
         self.assertEqual(bill.price, price)
 
     def test_increase(self):
         price = 100
-        bill = Bill("Teste", datetime.now(), price)
+        bill = Bill(description="Teste", due_date=datetime.now(), price=price)
 
         bill.increase(price)
 
@@ -21,14 +21,15 @@ class BillTest(unittest.TestCase):
 
     def test_increase_exception(self):
         decrease_value = -100.20
-        bill = Bill("Teste", datetime.now(), 100)
+        bill = Bill(description="Teste", due_date=datetime.now(), price=100)
 
         self.assertRaises(ValueError, bill.increase, decrease_value)
 
     def test_decrease(self):
         decrease_value = 100.20
         result = 20.00
-        bill = Bill("Teste", datetime.now(), 120.20)
+        bill = Bill(description="Teste",
+                    due_date=datetime.now(), price=120.20)
 
         bill.decrease(decrease_value)
 
@@ -36,12 +37,14 @@ class BillTest(unittest.TestCase):
 
     def test_decrease_exception(self):
         decrease_value = -100.20
-        bill = Bill("Teste", datetime.now(), 100)
+        bill = Bill(description="Teste", due_date=datetime.now(), price=100)
+
+        print(bill.id)
 
         self.assertRaises(ValueError, bill.decrease, decrease_value)
 
     def test_decrease_zero_price_exception(self):
-        bill = Bill("Teste", datetime.now(), 20)
+        bill = Bill(description="Teste", due_date=datetime.now(),  price=20)
 
         self.assertRaises(ValueError, bill.decrease, 100)
 
